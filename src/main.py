0014e2c3922775ae7b247a5df13efa1220409571
Edvard Majakari <edvard@majakari.net>
2022-03-17 18:55:26 +0200

    initial version

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import asyncio
+from databases import Database
+import os
+
+DBHOST = os.getenv("DBHOST", "localhost")
+DBUSER = os.getenv("DBUSER", "postgres")
+DBPOOL_MIN = os.getenv("DBPOOL_MIN", "2")
+DBPOOL_MAX = os.getenv("DBPOOL_MAX", "8")
+database = Database(
+    f"postgresql://{DBUSER}@{DBHOST}/test?min_size={DBPOOL_MIN}&max_size={DBPOOL_MAX}"
+)
+
+
+async def main():
+    await database.connect()
+
+    query = """CREATE TABLE IF NOT EXISTS mytbl (
+        id SERIAL, key VARCHAR(32)
+    )
+    """
+    await database.execute(query=query)
+
+    query = "INSERT INTO mytbl(key) VALUES (:key)"
+    values = ["fi", "fo", "fum"]
+    await database.execute_many(query=query, values=[{"key": val} for val in values])
+
+    query = "SELECT key FROM mytbl"
+    rows = await database.fetch_all(query=query)
+    print("DB now contains {} rows".format(len(rows)))
+
+asyncio.run(main())
